a18fc0c9eb9838b94d1b1d69541f5e7f00bfc7cb
ponyesteves <ajesteves@gmail.com>
2017-10-18 09:27:29 -0300

    ionicos ^2.0 con versiones superiores no funciona. Agrego a dependencia

--- templates/globals/admin-lte-css.js
+++ templates/globals/admin-lte-css.js
@@ -7,8 +7,3 @@ import 'admin-lte/dist/css/skins/_all-skins.min.css'
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css'
 import 'bootstrap-daterangepicker/daterangepicker.css'
 import './admin-lte-fonts.css'
-
-
-// import 'admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'
-// import 'jvectormap/jquery-jvectormap.css'
-// import '../../node_modules/morris.js/morris.css'
